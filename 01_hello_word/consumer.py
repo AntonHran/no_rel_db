ea38ba43fe4faf3c44c006f813bd83bde0e91078
AntonHran <for.program.learning@gmail.com>
2024-03-06 23:23:01 +0200

    function added

---
+++ 01_hello_word/consumer.py
@@ -0,0 +1,32 @@
+import os
+import sys
+
+import pika
+
+
+def main():
+    credentials = pika.PlainCredentials("guest", "guest")
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host="localhost", port=5672, credentials=credentials))
+    chanel = connection.channel()
+
+    chanel.queue_declare(queue="hello")
+
+    def callback(ch, method, body):
+        print(f" [x] Received: {body.decode()}")
+
+    chanel.basic_consume(queue="hello", on_message_callback=callback, auto_ack=True)
+
+    print(" [x] Waiting for messages. To exit press CTRL+C")
+    chanel.stop_consuming()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Interrupted")
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
