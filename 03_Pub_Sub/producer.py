8faa142f78b581bf9375e999d195a13c14286c63
AntonHran <for.program.learning@gmail.com>
2024-03-07 23:26:34 +0200

    edited

--- 03_Pub_Sub/producer.py
+++ 03_Pub_Sub/producer.py
@@ -8,20 +8,18 @@ credentials = pika.PlainCredentials("guest", "guest")
 connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost", port=5672, credentials=credentials))
 chanel = connection.channel()
 
-chanel.exchange_declare(exchange="Exchange_Name", exchange_type="direct")
-chanel.queue_declare(queue="queue_name", durable=True)
-chanel.queue_bind(exchange="Exchange_Name", queue="queue_name")
+chanel.exchange_declare(exchange="Events Message", exchange_type="fanout")
 
 
-def create_task(nums: int):
-    for i in range(nums):
-        message = {
-            "id": i,
-            "payload": f"Date: {datetime.now().isoformat()}"
-        }
-        chanel.basic_publish(exchange="Exchange_Name", routing_key="queue_name", body=json.dumps(message).encode())
+def create_event():
+    message = {
+        "event": "Test event",
+        "message": "Test message",
+        "payload": f"Date: {datetime.now().isoformat()}"
+    }
+    chanel.basic_publish(exchange="Events Message", routing_key="", body=json.dumps(message).encode())
     connection.close()
 
 
 if __name__ == '__main__':
-    create_task(100)
+    create_event()
