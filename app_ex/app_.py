6cade593babc149b9bfd56bf88f214fd2f2bf490
AntonHran <for.program.learning@gmail.com>
2024-03-08 00:29:26 +0200

    rewrite

--- app_ex/app_.py
+++ app_ex/app_.py
@@ -1,9 +1,9 @@
-import json
-from datetime import datetime
 import configparser
 import pathlib
 
 import pika
+
+from app_ex.models import Task
 
 
 file_config = pathlib.Path(__file__).parent.parent.joinpath("config.ini")
@@ -12,7 +12,10 @@ config.read(file_config)
 
 user = config.get("CloudRMQ", "USER")
 password = config.get("CloudRMQ", "PASSWORD")
-host = config.get("CloudRMQ", "DB_NAME")
+host = config.get("CloudRMQ", "HOST")
+
+exchange_name = "Exchange_Service"
+queue_name = "m8"
 
 credentials = pika.PlainCredentials(user, password)
 connection = pika.BlockingConnection(pika.ConnectionParameters(host=host,
@@ -21,20 +24,19 @@ connection = pika.BlockingConnection(pika.ConnectionParameters(host=host,
                                                                virtual_host=user))
 chanel = connection.channel()
 
-chanel.exchange_declare(exchange="Exchange_Service", exchange_type="direct")
-chanel.queue_declare(queue="m8", durable=True)
-chanel.queue_bind(exchange="Exchange_Service", queue="m8")
+chanel.exchange_declare(exchange=exchange_name, exchange_type="direct")
+chanel.queue_declare(queue=queue_name, durable=True)
+chanel.queue_bind(exchange=exchange_name, queue=queue_name)
 
 
 def create_task(nums: int):
     for i in range(nums):
-        message = {
-            "id": i,
-            "payload": f"Date: {datetime.now().isoformat()}"
-        }
-        chanel.basic_publish(exchange="Exchange_Name", routing_key="queue_name", body=json.dumps(message).encode())
+        task = Task(consumer="No name").save()
+        chanel.basic_publish(exchange=exchange_name, routing_key=queue_name,
+                             body=str(task.id).encode(),
+                             properties=pika.BasicProperties(delivery_mode=pika.spec.PERSISTENT_DELIVERY_MODE))
     connection.close()
 
 
 if __name__ == '__main__':
-    create_task(100)
+    create_task(1)
