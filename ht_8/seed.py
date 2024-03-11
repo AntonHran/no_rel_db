b24c01a5221bd35f99c870d7e0f7e581ff9a0872
AntonHran <for.program.learning@gmail.com>
2024-03-11 23:30:20 +0200

    init

---
+++ ht_8/seed.py
@@ -0,0 +1,34 @@
+import json
+
+from mongoengine.errors import NotUniqueError
+
+from models import Author, Quote
+
+
+def seed_authors(filename: str) -> None:
+    with open(filename, encoding="utf-8") as fd:
+        data: list[dict] = json.load(fd)
+        for el in data:
+            try:
+                author = Author(fullname=el.get("fullname"), born_date=el.get("born_date"),
+                                born_location=el.get("born_location"),
+                                description=el.get("description"))
+                author.save()
+            except NotUniqueError:
+                print(f"Author already exists: {el.get('fullname')}")
+
+
+def seed_quotes(filename: str) -> None:
+    with open(filename, encoding="utf-8") as fd:
+        data: list[dict] = json.load(fd)
+        for el in data:
+            author, _ = Author.objects(fullname=el.get("author"))
+            quote = Quote(author=author,
+                          tags=el.get("tags"),
+                          quote=el.get("quote"))
+            quote.save()
+
+
+if __name__ == '__main__':
+    seed_authors("authors.json")
+    seed_quotes("quotes.json")
