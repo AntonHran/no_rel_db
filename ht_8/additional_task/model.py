841f63fb60f825cf34f42821271a048b0b25595c
AntonHran <for.program.learning@gmail.com>
2024-03-17 01:55:16 +0200

    init

---
+++ ht_8/additional_task/model.py
@@ -0,0 +1,15 @@
+from mongoengine import connect, Document, StringField, BooleanField
+
+from app import uri
+
+
+connect(db="quotes", host=uri)
+
+
+class User(Document):
+    fullname = StringField(required=True, unique=True)
+    phone_number = StringField(max_length=30)
+    email = StringField(max_length=100)
+    email_sent = BooleanField(default=False)
+    position = StringField(max_length=30)
+    meta = {"collection": "users"}
