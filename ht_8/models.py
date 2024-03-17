76898495c96219aa2458cd413e3c21e5b9c64215
AntonHran <for.program.learning@gmail.com>
2024-03-17 21:17:02 +0200

    wdited

--- ht_8/models.py
+++ ht_8/models.py
@@ -1,5 +1,6 @@
 from bson import json_util
 from mongoengine import connect, Document, StringField, ReferenceField, ListField, CASCADE
+from mongoengine.errors import DoesNotExist
 
 from app import uri
 
@@ -8,7 +9,7 @@ connect(db="quotes", host=uri)
 
 
 class Author(Document):
-    fullname = StringField(required=True, unique=True)
+    full_name = StringField(required=True, unique=True)
     born_date = StringField(max_length=30)
     born_location = StringField(max_length=100)
     description = StringField()
@@ -17,11 +18,14 @@ class Author(Document):
 
 class Quote(Document):
     author = ReferenceField(Author, reverse_delete_rule=CASCADE)
-    tags = ListField(StringField(max_length=20))
+    tags = ListField(StringField(max_length=40))
     quote = StringField(required=True, unique=True)
     meta = {"collection": "quotes"}
 
     def to_json(self, *args, **kwargs):
         data = self.to_mongo(*args, **kwargs)
-        data["author"] = self.author.fullname
-        return json_util.dumps(data, ensure_ascii=False)
+        try:
+            data["author"] = self.author.full_name
+            return json_util.dumps(data, ensure_ascii=False)
+        except DoesNotExist:
+            print(f"There is not such author with id: {self.author}")
